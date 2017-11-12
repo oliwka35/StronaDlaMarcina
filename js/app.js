bd4a8c5e0b7314bd700e8c49ae19dea80bad37ed
oliwka35 <oliwka35@users.noreply.github.com>
2017-11-12 21:21:18 +0100

    Strona dla Marcina beta

---
+++ js/app.js
@@ -0,0 +1,22 @@
+    'use strict';
+
+
+    document.addEventListener('DOMContentLoaded', function() {
+
+
+
+        var forDropdown = document.querySelector('.for-dropdown');
+        var dropdown = document.querySelector('.dropdown');
+
+
+        forDropdown.addEventListener('mouseover', function() {
+            dropdown.style.display = 'block';
+            });
+
+        forDropdown.addEventListener('mouseout', function() {
+            dropdown.style.display = 'none';
+        });
+
+
+
+    });
